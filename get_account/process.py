909d3c467857c3e9df9c94cfaf98b57a80df416c
samyhama <samyhama@gmail.com>
2020-10-23 18:24:50 +0200

    First script to dl images from IG ok

--- get_account/process.py
+++ get_account/process.py
@@ -1,6 +1,11 @@
-from InstagramAPI import InstagramAPI
+from instagrapi import Client
 
-api = InstagramAPI("cutecats_cm", "JcbzAntho12.Cutecats")
-api.login()
-print(api.getProfileData())
+ACCOUNT_USERNAME = "cutecats_cm"
+ACCOUNT_PASSWORD = "JcbzAntho12.Cutecats"
 
+cl = Client()
+cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
+
+cl.album_upload(
+    ["data/tests/0c2b3cfd-6ad4-4ccd-bb32-b4c200cd03d5.jpg",
+     "data/tests/1c0e644f-6eae-4712-82db-6a4a453ef7d6.jpg"], caption="First post")
--- get_account/public_api.py
+++ get_account/public_api.py
@@ -1,4 +1,5 @@
 import requests
+import uuid
 
 
 class Parser:
@@ -9,6 +10,7 @@ class Parser:
     NODE_KEY = "node"
     CAPTION_LIST_KEY = "edge_media_to_caption"
     TEXT_KEY = "text"
+    URL_KEY = "display_url"
 
     def __init__(self, tag):
         self.tag = tag
@@ -22,22 +24,23 @@ class Parser:
         data = r.json()
         return data
 
-    #data['graphql']['hashtag']['edge_hashtag_to_media']['edges'][0]['node']['display_url']
-    def get_captions(self):
-        captions = []
+    def save_image_from_link(self, image_url):
+        img_data = requests.get(image_url).content
+        with open('data/cats/{}.jpg'.format(uuid.uuid4()), 'wb') as handler:
+            handler.write(img_data)
+
+    def get_images(self):
         data = self.get_request_response()
         nodes_list = data[Parser.HASH_KEY][Parser.HASHTAG_KEY][Parser.MEDIA_KEY][Parser.LIST_KEY]
         for obj in nodes_list:
-            caption_list = obj[Parser.NODE_KEY][Parser.CAPTION_LIST_KEY][Parser.LIST_KEY]
-            if len(caption_list) > 0:
-                caption = caption_list[0][Parser.NODE_KEY][Parser.TEXT_KEY]
-                captions.append(caption)
-                print(caption)
+            url = obj[Parser.NODE_KEY][Parser.URL_KEY]
+            if len(url) > 0:
+                self.save_image_from_link(url)
 
 
 def main():
     parser = Parser("cats")
-    parser.get_captions()
+    parser.get_images()
 
 
 if __name__ == "__main__":
